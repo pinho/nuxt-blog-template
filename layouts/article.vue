f15e3239d0a9beb0942f2809ce9d5a6207a8776d
Ronaldd Pinho <ronaldppinho@gmail.com>
2020-06-08 13:17:33 -0300

    Fix article.vue

--- layouts/article.vue
+++ layouts/article.vue
@@ -10,9 +10,7 @@ export default {
 }
 </script>
 
-<style lang="scss">
-// $background: #222;
-
+<style>
 #articleContainer {
   width: 70%;
   max-width: 920px;
@@ -20,36 +18,30 @@ export default {
   margin: 0 auto;
   padding: 48px 5rem;
   border: 1px solid #aaa;
-
-  .markdown-body {
-    font-family: 'Times New Roman', Times, serif;
-
-    h1 {
-      text-align: center;
-      font-size: 40px;
-      font-weight: 600;
-      padding-block: 2rem;
-    }
-
-    h2::before {
-      content: '## ';
-    }
-
-    p {
-      text-align: justify;
-      font-size: 13pt;
-      font-family: 'Fira Sans';
-      margin-block: .8rem;
-      line-height: 1.5rem;
-      // border: 1px solid blue;
-    }
-
-    a {
-      color: grey;
-    }
-    a:hover {
-      color: orangered;
-    }
-  }
+}
+.markdown-body {
+  font-family: 'Times New Roman', Times, serif;
+}
+.markdown-body > h1 {
+  text-align: center;
+  font-size: 40px;
+  font-weight: 600;
+  padding-block: 2rem;
+}
+.markdown-body > h2::before {
+  content: '## ';
+}
+.markdown-body > p {
+  text-align: justify;
+  font-size: 13pt;
+  font-family: 'Fira Sans';
+  margin-block: .8rem;
+  line-height: 1.5rem;
+}
+.markdown-body > a {
+  color: grey;
+}
+.markdown-body > a:hover {
+  color: orangered;
 }
 </style>
