dcb06526fb82788d75c747edeaf24cda111bdc7c
Ronaldd Pinho <ronaldppinho@gmail.com>
2020-06-08 13:53:42 -0300

    Add style in article template

--- layouts/article.vue
+++ layouts/article.vue
@@ -17,10 +17,9 @@ export default {
   min-width: 720px;
   margin: 0 auto;
   padding: 48px 5rem;
-  border: 1px solid #aaa;
 }
 .markdown-body {
-  font-family: 'Times New Roman', Times, serif;
+  font-family: 'Quicksand', 'Fira Sans', Arial, sans-serif;
 }
 .markdown-body > h1 {
   text-align: center;
@@ -35,13 +34,37 @@ export default {
   text-align: justify;
   font-size: 13pt;
   font-family: 'Fira Sans';
-  margin-block: .8rem;
+  margin-block: 1.2rem;
   line-height: 1.5rem;
 }
-.markdown-body > a {
+.markdown-body a {
   color: grey;
 }
-.markdown-body > a:hover {
+.markdown-body a:hover {
   color: orangered;
+}
+.markdown-body blockquote {
+  background-color: #ddd;
+  font-style: italic;
+  padding: 1.1rem 2rem;
+  border-left: 20px solid #3b8070;
+  box-sizing: content-box;
+}
+.markdown-body code {
+  box-sizing: border-box;
+  font-family: 'Fira Code', 'Source Code Pro', monospace, sans-serif;
+  font-size: 12pt;
+  color: #3b8070;
+}
+.markdown-body > pre {
+  width: 100%;
+  background-color: #1b3220;
+  color: lightgreen;
+  padding: 1rem;
+  border-radius: .3rem;
+  overflow-x: scroll;
+}
+.markdown-body > pre > code {
+  color: whitesmoke;
 }
 </style>
